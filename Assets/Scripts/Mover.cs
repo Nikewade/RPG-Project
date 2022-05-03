2d2573f0bf07384dc23e791a5e91b837b9447a3f
Nikewade <nikewade1006@gmail.com>
2022-05-03 05:02:14 -0400

    Camera tracking Post Process Navmesh click movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,9 +7,26 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    Ray lastRay;
+
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().SetDestination(target.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().SetDestination(hit.point);
+        }
     }
 }
